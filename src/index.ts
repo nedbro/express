d257b5e93600baa68747edc5b344c290ae6a4204
Kocsis Robert <robertzkocsis@gmail.com>
2023-01-25 18:28:23 +0100

    Add really basic endpoints

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,26 @@
 import * as express from 'express'
 import {Express, Request, Response} from 'express';
 
-
 const app: Express = express();
+app.use(express.json());
+
 
 const port = 4000;
+const basePath = "/api";
 
-app.get('/', (req: Request, res: Response) => {
+app.get(`${basePath}/posts/:id`, (req: Request, res: Response) => {
+  res.send('Express + TypeScript Server asdasdasd');
+});
+
+app.post(`${basePath}/posts`, (req: Request, res: Response) => {
+  res.send('Express + TypeScript Server asdasdasd');
+});
+
+app.put(`${basePath}/posts/:id`, (req: Request, res: Response) => {
+  res.send('Express + TypeScript Server asdasdasd');
+});
+
+app.delete(`${basePath}/posts/:id`, (req: Request, res: Response) => {
   res.send('Express + TypeScript Server asdasdasd');
 });
 
