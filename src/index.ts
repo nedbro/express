74c7a20236395c73f4476b59c83b5cf835bc7b74
Kocsis Robert <robertzkocsis@gmail.com>
2023-01-25 19:13:03 +0100

    Add basic id validation

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,26 @@
 import * as express from 'express'
-import {Express, Request, Response} from 'express';
+import { Express, NextFunction, Request, Response } from 'express';
+import { param, Result, ValidationChain, ValidationError, validationResult } from 'express-validator';
 
 const app: Express = express();
 app.use(express.json());
 
-
 const port = 4000;
 const basePath = "/api";
 
-app.get(`${basePath}/posts/:id`, (req: Request, res: Response) => {
+class AppError extends Error {
+  statusCode: number;
+
+  constructor(statusCode: number, message: string) {
+    super(message);
+    this.statusCode = statusCode;
+  }
+}
+
+const idValidator: ValidationChain = param('id').exists().toInt().custom(value => !isNaN(value));
+
+app.get(`${basePath}/posts/:id`, idValidator, (req: Request, res: Response) => {
+  validateInput(req, res);
   res.send('Express + TypeScript Server asdasdasd');
 });
 
@@ -16,14 +28,33 @@ app.post(`${basePath}/posts`, (req: Request, res: Response) => {
   res.send('Express + TypeScript Server asdasdasd');
 });
 
-app.put(`${basePath}/posts/:id`, (req: Request, res: Response) => {
+app.put(`${basePath}/posts/:id`, idValidator, (req: Request, res: Response) => {
+  validateInput(req, res);
   res.send('Express + TypeScript Server asdasdasd');
 });
 
-app.delete(`${basePath}/posts/:id`, (req: Request, res: Response) => {
+app.delete(`${basePath}/posts/:id`, idValidator, (req: Request, res: Response) => {
+  validateInput(req, res);
   res.send('Express + TypeScript Server asdasdasd');
 });
+
+const errorHandler = (error: Error, req: Request, res: Response, next: NextFunction) => {
+  console.error("An error occurred", error.message)
+  return res.status(400).json({ errorMessage: error.message });
+}
+
+app.use(errorHandler);
 
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
 });
+
+
+const validateInput = (req: Request, res: Response): void => {
+  const errors: Result<ValidationError> = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    const errorMessage: string = Object.entries(errors.mapped()).map(entrySet => `${entrySet[0]}: ${entrySet[1].msg}`).join(",");
+    throw new AppError(400, errorMessage);
+  }
+}
